0c6983af5481d55b057056448754e36899fbc69b
KotelnikovM <kotelnikovofficial@gmail.com>
2024-02-07 22:26:14 +0500

    add: add main container of todo-app

---
+++ todo-app/src/app/app.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { TodoApp } from '../widgets/todo-app';
+
+function App() {
+  return <TodoApp />;
+}
+
+export default App;
---
+++ todo-app/src/widgets/todo-app/ui/index.tsx
@@ -0,0 +1,5 @@
+import styles from './index.module.css';
+
+export function TodoApp() {
+  return <div className={styles.container}>index</div>;
+}
